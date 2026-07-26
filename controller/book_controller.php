979fc15fd652db498edd9048b4602324c3241636
Stanley <Seungsu97233@gmail.com>
2026-07-27 00:36:23 +0200

    supp view

--- controller/book_controller.php
+++ controller/book_controller.php
@@ -9,11 +9,19 @@ function add_book() {
     //gestion du formulaire
     if (isset($_POST["submit"])) {
         //vérifier si les champs existe
+    // champ manquant ou vide → erreur
+    if (!empty($_POST["title"]) && !empty($_POST["summary"]) && !empty($_POST["author"]) && !empty($_POST["published_at"]) && !empty($_POST["category_id"])) { 
         //nettoyer les données
-        //tester si le champs summary est plus petit que 255
-        //ajouter à la bdd
-        //afficher un message
-        $message = "";
+            $title = trim($_POST["title"]);
+            $summary = trim($_POST["summary"]);
+            $author = trim($_POST["author"]);
+            $published_at = trim($_POST["published_at"]);
+            $category_id = (int) $_POST["category_id"];
+    //tester si le champs summary est plus petit que 255
+    if(strlen($summary)<255){}
     }
-    include '../view/template_add_book.php';
+    }
 }
+        // //ajouter à la bdd
+        //afficher un message
+    include '../view/template_add_book.php';
